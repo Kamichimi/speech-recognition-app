b60c72d9627edc2d6026d984f1b6a75e4ef7285a
Kamichimi <breville.alexandre77@gmail.com>
2024-06-24 10:23:38 +0200

    speech to text completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, NgZone } from '@angular/core';
+declare var window: any;
+
+const webkitSpeechRecognition = window.webkitSpeechRecognition;
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class AppComponent {
-  title = 'speech-recognition-app';
+  transcript = '';
+  isListening = false;
+  constructor(private ngZone: NgZone) { 
+    console.log('AppComponent constructor called');
+  }
+
+  startListening() {
+    console.log('startListening called');
+    this.isListening = true;
+    console.log('Écoute démarrée');
+
+    
+    const recognition = new webkitSpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.lang = 'fr-FR';
+    recognition.maxResults = 10;
+
+    recognition.onresult = (event: { results: { transcript: any; }[][]; }) => {
+      this.ngZone.run(() => {
+        this.transcript = '';
+        const transcript = event.results[0][0].transcript;
+        console.log(`Vous dites : ${transcript}`);
+        this.transcript = transcript;
+      });
+    };
+    recognition.onend = () => {
+      this.ngZone.run(() => {
+        console.log('Écoute terminée');
+        this.isListening = false;
+        setTimeout(() => {
+          console.log('Retransmission du texte :', this.transcript);
+        }, 1000);
+      });
+    };
+    recognition.start();
+  }
+
+  doSomething(){
+    console.log("recherche")
+  }
 }
